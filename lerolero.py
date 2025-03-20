791a070d57e6f17f7ba8047e34965fa11ca64bf9
GustavoB <gustavo.barbeiro@usp.br>
2025-03-20 10:56:20 -0300

    Insere os textos em português

--- lerolero.py
+++ lerolero.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 """Gerador de lero-lero.
 
-Gera frases de efeito sem significado real."""
+...Gera frases de efeito sem significado real."""
 
 import random
 
 # Cada frase é composta por três partes aleatórias;
-# Listas de possibilidades
+# Listas de possibilidade para cada uma das partes.
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = [
+	"O sistema em desenvolvimento",
+	"O novo protocolo de comunicação",
+	"O algoritmo otimizado"
+	]
+parte2 = [
+	"possui excelente desenpenho",
+	"oferece garantias de precisão acima da média"
+	"preenche uma lacuna significatica"
+	]
+parte3 = [
+	"nas aplicações a que se destina",
+	"em relação às opções disponíveis no mercado",
+	", promovendo ampla vantagem competitica a seus usuários"
+	]
 
-# Imprimi em ordem aleatória
+# Combina as partes aleatoriamente
 
 print (random.choice(parte1), random.choice(parte2), rondom.choice(parte3))
