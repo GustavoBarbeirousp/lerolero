083a90c12b4ee2cd72c3e48fc6588fdd9fe2135d
GustavoB <gustavo.barbeiro@usp.br>
2025-03-20 10:33:32 -0300

    Implementa esqueleto do programa

--- lerolero.py
+++ lerolero.py
@@ -2,3 +2,16 @@
 """Gerador de lero-lero.
 
 Gera frases de efeito sem significado real."""
+
+import random
+
+# Cada frase é composta por três partes aleatórias;
+# Listas de possibilidades
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+# Imprimi em ordem aleatória
+
+print (random.choice(parte1), random.choice(parte2), rondom.choice(parte3))
